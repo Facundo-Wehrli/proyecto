bec7736750a67c2ef1d30b2e9fe14ca82557a8e9
JoanRossi <elpibedelapc@gmail.com>
2023-01-11 08:10:55 -0300

    push proyecto PPeluqueria

---
+++ PPeluqueria/peluqueria/src/main/java/com/example/peluqueria/PeluqueriaApplication.java
@@ -0,0 +1,15 @@
+package com.example.peluqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluqueriaApplication.class, args);
+
+		
+	}
+
+}
